3a734907bf1481e5ff385e61635a22980f052fd6
maxhu08 <contact@maxhu.dev>
2025-02-02 14:10:34 -0600

    export data to csv

--- src/csv.ts
+++ src/csv.ts
@@ -9,15 +9,22 @@ const gencsv = async () => {
 
   const fileNames = await readdir(folderPath, { recursive: true });
 
-  let csvContent = "";
+  const csvLines = ["name,15s wpm,15s raw,60s wpm,60s raw,10w wpm"];
 
   for (const fileName of fileNames) {
-    csvContent += `${fileName}\n`;
+    const fileJson = await Bun.file(`results/data/${date}/${fileName}`).json();
 
-    console.log(fileName);
+    const name = fileName.replace(/\.json$/, "");
+    const wpm15s = fileJson.data.personalBests.time["15"][0].wpm || "";
+    const raw15s = fileJson.data.personalBests.time["15"][0].raw || "";
+    const wpm60s = fileJson.data.personalBests.time["60"][0].wpm || "";
+    const raw60s = fileJson.data.personalBests.time["60"][0].raw || "";
+    const wpm10w = fileJson.data.personalBests.words["10"][0].wpm || "";
+
+    csvLines.push(`${name},${wpm15s},${raw15s},${wpm60s},${raw60s},${wpm10w}`);
   }
 
-  await writeFile(`results/csv/${date}.csv`, csvContent);
+  await writeFile(`results/csv/${date}.csv`, csvLines.join("\n"));
 };
 
 gencsv().catch(console.error);
